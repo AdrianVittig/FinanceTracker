bb275ed7e3333c3d079ac70bd23822fe8e1a8849
AdriannVitig <168094880+AdriannVitig@users.noreply.github.com>
2024-10-30 14:38:41 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,94 @@
+const movementsArr = [250, 130, -500, 940, -80, 265, -1.39, 400];
+
+const datesArr = [
+  "2024-09-08",
+  "2024-10-17",
+  "2023-08-15",
+  "2024-03-05",
+  "2024-05-19",
+  "2024-07-03",
+  "2023-01-01",
+  "2024-10-19",
+  "2024-10-30",
+];
+
+const balanceLabel = document.querySelector(".balance");
+
+const transactionsList = document.querySelector(".recent-transactions-list");
+
+const totalIncomeLabel = document.querySelector(".total-income");
+
+const totalExpensesLabel = document.querySelector(".total-expenses");
+
+function formatCurrency(amount) {
+  return `${Math.abs(amount.toFixed(2))} лв`;
+}
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return new Date(dateString).toLocaleDateString("bg-BG", options);
+}
+
+function updateIncome() {
+  const totalIncome = movementsArr
+    .filter(function (mov) {
+      return mov > 0;
+    })
+    .reduce(function (acc, mov) {
+      return (acc += mov);
+    });
+  totalIncomeLabel.textContent = `Доходи: ${totalIncome.toFixed(2)} лв`;
+}
+
+function updateExpenses() {
+  const totalExpenses = movementsArr
+    .filter(function (mov) {
+      return mov < 0;
+    })
+    .reduce(function (acc, mov) {
+      return (acc += mov);
+    });
+  totalExpensesLabel.textContent = `Разходи: ${Math.abs(
+    totalExpenses.toFixed(2)
+  )} лв.`;
+}
+
+function updateBalance() {
+  const totalBal = movementsArr.reduce(function (acc, mov) {
+    return (acc += mov);
+  });
+  balanceLabel.textContent = `Баланс: ${totalBal.toFixed(2)} лв`;
+}
+function displayMovements() {
+  transactionsList.innerHTML = "";
+
+  movementsArr.forEach(function (movement, index) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Дата: ${formatDate(
+      datesArr[index]
+    )}; Сума: ${formatCurrency(movement)}`;
+
+    listItem.style.padding = "1rem";
+    listItem.style.margin = "1rem";
+    listItem.style.borderRadius = "1rem";
+    listItem.style.fontWeight = "bold";
+    listItem.style.textAlign = "center";
+    if (movement < 0) {
+      listItem.style.backgroundColor = "rgb(199, 71, 71)";
+
+      listItem.style.color = "white";
+    } else {
+      listItem.style.backgroundColor = "rgb(52, 143, 66)";
+      listItem.style.color = "white";
+    }
+
+    transactionsList.appendChild(listItem);
+  });
+}
+function updateUI() {
+  displayMovements();
+  updateBalance();
+  updateIncome();
+  updateExpenses();
+}
+
+updateUI();
